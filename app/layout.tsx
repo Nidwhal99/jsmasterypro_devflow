5d8fc6039d3933bb60192bf7842cd027542948be
Antonyg99 <Antony.L.Guzman.22@dartmouth.edu>
2025-03-14 13:43:18 -0700

    Add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import ThemesProvider from "@/context/Theme";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -43,6 +44,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemesProvider>
       </body>
