cce1c69067bbbf62f499f850bc6c56b80894872d
Antonyg99 <Antony.L.Guzman.22@dartmouth.edu>
2025-05-22 01:23:33 -0700

    Implement Interaction Model

--- database/template.model.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model, models } from "mongoose";
-
-export type IModel = object;
-
-const ModelSchema = new Schema<IModel>({}, { timestamps: true });
-
-const Model = models?.Model || model<IModel>("Model", ModelSchema);
-
-export default Model;
